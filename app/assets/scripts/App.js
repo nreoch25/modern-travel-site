09863b4e17b077980f4aff7fe42c6825e2145dac
Nigel <nigelreoch@yahoo.com>
2016-10-22 10:53:50 -0400

    add js for mobileMenu

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,17 +1,3 @@
-import $ from "jquery";
-import Person from "./modules/Person";
+import MobileMenu from "./modules/MobileMenu";
 
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + " now owes $0 in taxes");
-  }
-}
-
-alert("Scripts Webpack");
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "green");
-jane.greet();
-jane.payTaxes();
+var mobileMenu = new MobileMenu();
